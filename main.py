511ff3863c8a5cd999562bcf360a796e5d6c0e60
Jeffrey <jeffreywu408@gmail.com>
2018-11-20 10:05:15 +0800

    commit

--- main.py
+++ main.py
@@ -41,9 +41,90 @@ def insertionSort(array):
         array[j+1] = key 
         
 
-#def heapSort(array):
+ 
+def heapSort(array):
+    if not array:
+        return
+    
+    Heapify(array)
 
-#def mergeSort(array):
+    # The array from [0, ... , i] is the heap
+    # The array from [i+1, ... , end] is in sorted order
+    for i in range(len(array) - 1, -1, -1):
+        array[0], array[i] = array[i], array[0]
+        siftDown(array, 0, i - 1);
+
+def Heapify(array):
+    # Root Node is at index 0
+    # Nth Node is at index (n + 1)
+    # Left Child is at index (2n + 1)
+    # Right Child is at index (2n + 2)
+
+    # Start with the parent of the last element
+    # Sift down the node to its proper place
+    for i in range((len(array) - 2) // 2, -1, -1):
+        siftDown(array, i, len(array) - 1);
+
+
+def siftDown(array, root, end) :
+    # Continually swap the root with the greatest child
+    # until theres's no more children or the root is greater than the children
+    while (2 * root + 1 <= end):
+        swap = root;
+        child = 2 * root + 1; # left child
+
+        # Check left child
+        if (array[child] > array[swap]):
+            swap = child
+
+        # Check right child
+        if ((child + 1 <= end) and (array[child + 1] > array[swap])):
+            swap = child + 1;
+
+        if (swap == root):
+            # The root is greater than its children
+            return;
+        else:
+            array[swap], array[root] = array[root], array[swap]
+
+            root = swap;
+
+def mergeSort(array):
+    if not array:
+        return
+    
+    if (len(array) > 1):
+        mid = len(array) // 2
+        left = array[:mid] 
+        right = array[mid:]
+    
+        mergeSort(left)
+        mergeSort(right)
+        merge(left, right, array)
+
+def merge(first, second, array):
+    indexFirst = 0;
+    indexSecond = 0;
+    j = 0;
+
+    while (indexFirst < len(first) and indexSecond < len(second)):
+        if (first[indexFirst] < second[indexSecond]):
+            array[j] = first[indexFirst]
+            indexFirst += 1
+        else:
+            array[j] = second[indexSecond]
+            indexSecond += 1
+        j += 1
+
+    while (indexFirst < len(first)):
+        array[j] = first[indexFirst]
+        indexFirst += 1
+        j += 1
+
+    while (indexSecond < len(second)):
+        array[j] = second[indexSecond]
+        indexSecond += 1
+        j += 1
 
 #def quickSort(array):
 
@@ -53,7 +134,7 @@ def isSorted(list):
     return bool(all(list[i] <= list[i+1] for i in range(len(list)-1)))
 
 def main():
-    size = 1000;
+    size = 10000;
     n = 1000;
     
     myList = [(random.randint(0, n)) for _ in range(size)]
@@ -74,6 +155,41 @@ def main():
     print("Insertion Sort")
     start = time.time()
     insertionSort(myList)
+    duration = time.time() - start
+    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    
+    myList = [(random.randint(0, n)) for _ in range(size)]
+    print("Heap Sort")
+    start = time.time()
+    heapSort(myList)
+    duration = time.time() - start
+    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    
+    myList = [(random.randint(0, n)) for _ in range(size)]
+    print("Merge Sort")
+    start = time.time()
+    mergeSort(myList)
+    duration = time.time() - start
+    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    
+    myList = [(random.randint(0, n)) for _ in range(size)]
+    print("Quick Sort")
+    start = time.time()
+    #quickSort(myList)
+    duration = time.time() - start
+    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    
+    myList = [(random.randint(0, n)) for _ in range(size)]
+    print("Quick Sort (3-Way Partition)")
+    start = time.time()
+    #quickSort3(myList)
+    duration = time.time() - start
+    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    
+    myList = [(random.randint(0, n)) for _ in range(size)]
+    print("Python Sort")
+    start = time.time()
+    myList.sort()
     duration = time.time() - start
     print("(%s): %f seconds\n" % (isSorted(myList), duration))
     
