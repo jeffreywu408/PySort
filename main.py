657da7a341aeb75031281e5c0f25ad1838e00339
Jeffrey <jeffreywu408@gmail.com>
2018-11-20 11:14:35 +0800

    fix

--- main.py
+++ main.py
@@ -20,12 +20,11 @@ def selectionSort(array):
         # Linear search for the minimum
         min = i 
         for j in range(i + 1, len(array)): 
-            if array[min] > array[j]: 
-                min_idx = j   
+            if array[j] < array[min]: 
+                min = j   
                 
         # Put minimum into correct place     
         array[i], array[min] = array[min], array[i] 
-        
 
 def insertionSort(array): 
     if not array:
@@ -39,8 +38,6 @@ def insertionSort(array):
             array[j+1] = array[j] 
             j -= 1
         array[j+1] = key 
-        
-
  
 def heapSort(array):
     if not array:
@@ -64,7 +61,6 @@ def Heapify(array):
     # Sift down the node to its proper place
     for i in range((len(array) - 2) // 2, -1, -1):
         siftDown(array, i, len(array) - 1);
-
 
 def siftDown(array, root, end) :
     # Continually swap the root with the greatest child
@@ -126,15 +122,92 @@ def merge(first, second, array):
         indexSecond += 1
         j += 1
 
-#def quickSort(array):
+def quickSort(array, i = None):
+    if not array:
+        return
+    
+    if i is None:
+        quickSort2(array, 0, len(array) - 1)
+    else:
+        quickSort3(array, 0, len(array) - 1)
+    
 
-#def quickSort3(array):
+def quickSort2(array, lowerIndex, higherIndex):
+    if not array:
+        return
+    
+    if (lowerIndex < higherIndex):
+        pivotIndex = partition(array, lowerIndex, higherIndex)
+
+        quickSort2(array, lowerIndex, pivotIndex - 1)
+        quickSort2(array, pivotIndex + 1, higherIndex)
+
+def partition(array, lowerIndex, higherIndex):
+    # Pick a pivot between lower index and higher index
+    pivotIndex = random.randint(lowerIndex, higherIndex)
+    
+    # Place pivot at the end of the subarray
+    array[higherIndex], array[pivotIndex] = array[pivotIndex], array[higherIndex]
+    
+    pivotIndex = higherIndex
+    pivot = array[pivotIndex]
+
+    # Partitioning
+    i = lowerIndex;
+    #for (int j = lowerIndex; j < higherIndex; j++):
+    for j in range(lowerIndex, higherIndex):
+        if (array[j] <= pivot):
+            array[i], array[j] = array[j], array[i]
+            i += 1
+
+    # Move the pivot into position i
+    array[i], array[pivotIndex] = array[pivotIndex], array[i]
+
+    return i;
+
+def quickSort3(array, lowerIndex, higherIndex):
+    if not array:
+        return
+    
+    if (lowerIndex < higherIndex):
+        # Pick a pivot between lower index and higher index
+        pivotIndex = random.randint(lowerIndex, higherIndex)
+
+        # Place pivot at the start of the sub-array
+        array[lowerIndex], array[pivotIndex] = array[pivotIndex], array[lowerIndex]
+
+        pivotIndex = lowerIndex
+        pivot = array[pivotIndex]
+
+        # The upper index of the "less than pivot" partition
+        lt = lowerIndex
+        # The lower index of the "greater than pivot" partition
+        gt = higherIndex
+
+        # Partitioning
+        i = lowerIndex + 1
+        while (i <= gt):
+            if (array[i] < pivot):
+                array[i], array[lt] = array[lt], array[i]
+                i += 1
+                lt += 1
+            
+            elif (array[i] > pivot):
+                array[i], array[gt] = array[gt], array[i]
+                gt -= 1
+
+            else:
+                i += 1
+
+        # Recursively call on the left and right partitions
+        quickSort3(array, lowerIndex, lt - 1);
+        quickSort3(array, gt + 1, higherIndex);
 
 def isSorted(list):
     return bool(all(list[i] <= list[i+1] for i in range(len(list)-1)))
 
 def main():
-    size = 10000;
+    size = 1000;
     n = 1000;
     
     myList = [(random.randint(0, n)) for _ in range(size)]
@@ -175,14 +248,14 @@ def main():
     myList = [(random.randint(0, n)) for _ in range(size)]
     print("Quick Sort")
     start = time.time()
-    #quickSort(myList)
+    quickSort(myList)
     duration = time.time() - start
     print("(%s): %f seconds\n" % (isSorted(myList), duration))
     
     myList = [(random.randint(0, n)) for _ in range(size)]
     print("Quick Sort (3-Way Partition)")
     start = time.time()
-    #quickSort3(myList)
+    quickSort(myList, 3)
     duration = time.time() - start
     print("(%s): %f seconds\n" % (isSorted(myList), duration))
     
