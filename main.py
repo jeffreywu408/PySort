4c7a559aac3d3f3a81880880e5638f56f2507936
Jeffrey <jeffreywu408@gmail.com>
2019-02-02 11:22:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import time
 
 
-def bubbleSort(array):
+def bubble_sort(array):
     if not array:
         return
 
@@ -14,22 +14,22 @@ def bubbleSort(array):
                 array[j], array[j + 1] = array[j + 1], array[j]
 
 
-def selectionSort(array):
+def selection_sort(array):
     if not array:
         return
 
     for i in range(len(array)):
         # Linear search for the minimum
-        min = i
+        minimum = i
         for j in range(i + 1, len(array)):
-            if array[j] < array[min]:
-                min = j
+            if array[j] < array[minimum]:
+                minimum = j
 
         # Put minimum into correct place
-        array[i], array[min] = array[min], array[i]
+        array[i], array[minimum] = array[minimum], array[i]
 
 
-def insertionSort(array):
+def insertion_sort(array):
     if not array:
         return
 
@@ -43,20 +43,20 @@ def insertionSort(array):
         array[j + 1] = key
 
 
-def heapSort(array):
+def heap_sort(array):
     if not array:
         return
 
-    Heapify(array)
+    heapify(array)
 
     # The array from [0, ... , i] is the heap
     # The array from [i+1, ... , end] is in sorted order
     for i in range(len(array) - 1, -1, -1):
         array[0], array[i] = array[i], array[0]
-        siftDown(array, 0, i - 1)
+        sift_down(array, 0, i - 1)
 
 
-def Heapify(array):
+def heapify(array):
     # Root Node is at index 0
     # Nth Node is at index (n + 1)
     # Left Child is at index (2n + 1)
@@ -65,125 +65,125 @@ def Heapify(array):
     # Start with the parent of the last element
     # Sift down the node to its proper place
     for i in range((len(array) - 2) // 2, -1, -1):
-        siftDown(array, i, len(array) - 1);
+        sift_down(array, i, len(array) - 1)
 
 
-def siftDown(array, root, end):
+def sift_down(array, root, end):
     # Continually swap the root with the greatest child
-    # until theres's no more children or the root is greater than the children
-    while (2 * root + 1 <= end):
-        swap = root;
+    # until there's no more children or the root is greater than the children
+    while 2 * root + 1 <= end:
+        swap = root
         child = 2 * root + 1  # left child
 
         # Check left child
-        if (array[child] > array[swap]):
+        if array[child] > array[swap]:
             swap = child
 
         # Check right child
-        if ((child + 1 <= end) and (array[child + 1] > array[swap])):
+        if (child + 1 <= end) and (array[child + 1] > array[swap]):
             swap = child + 1
 
-        if (swap == root):
+        if swap == root:
             # The root is greater than its children
-            return;
+            return
         else:
             array[swap], array[root] = array[root], array[swap]
             root = swap
 
 
-def mergeSort(array):
+def merge_sort(array):
     if not array:
         return
 
-    if (len(array) > 1):
+    if len(array) > 1:
         mid = len(array) // 2
         left = array[:mid]
         right = array[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+        merge_sort(left)
+        merge_sort(right)
         merge(left, right, array)
 
 
 def merge(first, second, array):
-    indexFirst = 0;
-    indexSecond = 0;
-    j = 0;
+    index_first = 0
+    index_second = 0
+    j = 0
 
-    while (indexFirst < len(first) and indexSecond < len(second)):
-        if (first[indexFirst] < second[indexSecond]):
-            array[j] = first[indexFirst]
-            indexFirst += 1
+    while index_first < len(first) and index_second < len(second):
+        if first[index_first] < second[index_second]:
+            array[j] = first[index_first]
+            index_first += 1
         else:
-            array[j] = second[indexSecond]
-            indexSecond += 1
+            array[j] = second[index_second]
+            index_second += 1
         j += 1
 
-    while (indexFirst < len(first)):
-        array[j] = first[indexFirst]
-        indexFirst += 1
+    while index_first < len(first):
+        array[j] = first[index_first]
+        index_first += 1
         j += 1
 
-    while (indexSecond < len(second)):
-        array[j] = second[indexSecond]
-        indexSecond += 1
+    while index_second < len(second):
+        array[j] = second[index_second]
+        index_second += 1
         j += 1
 
 
-def quickSort(array, lowerIndex=None, higherIndex=None):
+def quick_sort(array, lower_index=None, higher_index=None):
     if not array:
         return
 
-    if (lowerIndex is None and higherIndex is None):
-        quickSort(array, 0, len(array) - 1)
+    if lower_index is None and higher_index is None:
+        quick_sort(array, 0, len(array) - 1)
 
-    if (lowerIndex is not None) and (higherIndex is not None) and (lowerIndex < higherIndex):
-        pivotIndex = partition(array, lowerIndex, higherIndex)
-        quickSort(array, lowerIndex, pivotIndex - 1)
-        quickSort(array, pivotIndex + 1, higherIndex)
+    if (lower_index is not None) and (higher_index is not None) and (lower_index < higher_index):
+        pivot_index = partition(array, lower_index, higher_index)
+        quick_sort(array, lower_index, pivot_index - 1)
+        quick_sort(array, pivot_index + 1, higher_index)
 
 
 def partition(array, lowerIndex, higherIndex):
     # Pick a pivot between lower index and higher index
-    pivotIndex = random.randint(lowerIndex, higherIndex)
+    pivot_index = random.randint(lowerIndex, higherIndex)
 
     # Place pivot at the end of the subarray
-    array[higherIndex], array[pivotIndex] = array[pivotIndex], array[higherIndex]
+    array[higherIndex], array[pivot_index] = array[pivot_index], array[higherIndex]
 
-    pivotIndex = higherIndex
-    pivot = array[pivotIndex]
+    pivot_index = higherIndex
+    pivot = array[pivot_index]
 
     # Partitioning
     i = lowerIndex
-    # for (int j = lowerIndex; j < higherIndex; j++):
     for j in range(lowerIndex, higherIndex):
-        if (array[j] <= pivot):
+        if array[j] <= pivot:
             array[i], array[j] = array[j], array[i]
             i += 1
 
     # Move the pivot into position i
-    array[i], array[pivotIndex] = array[pivotIndex], array[i]
+    array[i], array[pivot_index] = array[pivot_index], array[i]
 
     # Return pivot index
     return i
 
 
-def quickSort3(array, lowerIndex=None, higherIndex=None):
+def quick_sort3(array, lowerIndex=None, higherIndex=None):
     if not array:
         return
 
-    if (lowerIndex is None and higherIndex is None):
-        quickSort3(array, 0, len(array) - 1)
+    if lowerIndex is None and higherIndex is None:
+        quick_sort3(array, 0, len(array) - 1)
 
-    if (lowerIndex is not None) and (higherIndex is not None) and (lowerIndex < higherIndex):
+    if (lowerIndex is not None) and (higherIndex is not None) \
+            and (lowerIndex < higherIndex):
         # Pick a pivot between lower index and higher index
-        pivotIndex = random.randint(lowerIndex, higherIndex)
+        pivot_index = random.randint(lowerIndex, higherIndex)
 
         # Place pivot at the start of the sub-array
-        array[lowerIndex], array[pivotIndex] = array[pivotIndex], array[lowerIndex]
+        array[lowerIndex], array[pivot_index] = array[pivot_index], array[lowerIndex]
 
-        pivotIndex = lowerIndex
-        pivot = array[pivotIndex]
+        pivot_index = lowerIndex
+        pivot = array[pivot_index]
 
         # The upper index of the "less than pivot" partition
         lt = lowerIndex
@@ -192,13 +192,13 @@ def quickSort3(array, lowerIndex=None, higherIndex=None):
 
         # Partitioning
         i = lowerIndex + 1
-        while (i <= gt):
-            if (array[i] < pivot):
+        while i <= gt:
+            if array[i] < pivot:
                 array[i], array[lt] = array[lt], array[i]
                 i += 1
                 lt += 1
 
-            elif (array[i] > pivot):
+            elif array[i] > pivot:
                 array[i], array[gt] = array[gt], array[i]
                 gt -= 1
 
@@ -206,11 +206,11 @@ def quickSort3(array, lowerIndex=None, higherIndex=None):
                 i += 1
 
         # Recursively call on the left and right partitions
-        quickSort3(array, lowerIndex, lt - 1)
-        quickSort3(array, gt + 1, higherIndex)
+        quick_sort3(array, lowerIndex, lt - 1)
+        quick_sort3(array, gt + 1, higherIndex)
 
 
-def isSorted(list):
+def is_sorted(list):
     return bool(all(list[i] <= list[i + 1] for i in range(len(list) - 1)))
 
 
@@ -218,61 +218,61 @@ def main():
     size = 1000
     n = 1000
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Bubble Sort")
     start = time.time()
-    bubbleSort(myList)
+    bubble_sort(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Selection Sort")
     start = time.time()
-    selectionSort(myList)
+    selection_sort(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Insertion Sort")
     start = time.time()
-    insertionSort(myList)
+    insertion_sort(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Heap Sort")
     start = time.time()
-    heapSort(myList)
+    heap_sort(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Merge Sort")
     start = time.time()
-    mergeSort(myList)
+    merge_sort(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Quick Sort")
     start = time.time()
-    quickSort(myList)
+    quick_sort(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Quick Sort (3-Way Partition)")
     start = time.time()
-    quickSort3(myList)
+    quick_sort3(my_list)
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
-    myList = [(random.randint(0, n)) for _ in range(size)]
+    my_list = [(random.randint(0, n)) for _ in range(size)]
     print("Python Sort")
     start = time.time()
-    myList.sort()
+    my_list.sort()
     duration = time.time() - start
-    print("(%s): %f seconds\n" % (isSorted(myList), duration))
+    print("(%s): %f seconds\n" % (is_sorted(my_list), duration))
 
 
 if __name__ == '__main__':
